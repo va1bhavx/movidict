c3157a1f8d5b8d052e8ccf47338d5014e28d7e43
Vaibhav <vaibhav2k1507@gmail.com>
2026-09-21 16:59:54 +0530

    feat: added cast and details in movie details page

--- components/website/movie-detail/components/detail-page-cast.tsx
+++ components/website/movie-detail/components/detail-page-cast.tsx
@@ -4,19 +4,25 @@ const cast = Array.from({ length: 10 })
 
 export default function DetailPageCast() {
   return (
-    <section className="w-full">
+    <section className="w-fullj">
       <div className="grid w-full shrink-0 grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
         {cast.map((_, index) => (
-          <div
-            key={index}
-            className="relative aspect-2/3 w-full overflow-hidden rounded-xl bg-neutral-900"
-          >
-            <Image
-              src="https://image.tmdb.org/t/p/w500/cO7J0XSVKPlAjUCMWC7DVBn1Py2.jpg"
-              alt=""
-              fill
-              className="object-cover transition-transform duration-300"
-            />
+          <div>
+            <div
+              key={index}
+              className="relative aspect-2/3 w-full overflow-hidden rounded-xl bg-neutral-900"
+            >
+              <Image
+                src="https://image.tmdb.org/t/p/w500/cO7J0XSVKPlAjUCMWC7DVBn1Py2.jpg"
+                alt=""
+                fill
+                className="object-cover transition-transform duration-300"
+              />
+            </div>
+            <div>
+              <h2 className="text-sm">John Doe.</h2>
+              <p className="text-xs text-muted-foreground">Actor</p>
+            </div>
           </div>
         ))}
       </div>
